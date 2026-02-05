131899ae68fb51041dce8dd157014e563f574113
katuishi <diogenes_mj@hotmail.com>
2026-02-04 23:05:45 -0400

    refactor: restructure process monitoring logic and improve status checks

--- src/core/deamon.py
+++ src/core/deamon.py
@@ -8,27 +8,44 @@ from process import Process
 
 class Daemon:
     def __init__(self):
-        self.process_manager = Process()
+        pass
 
     async def monitor_processes(self):
         """Monitor all processes and print their status."""
         while True:
-            for pid, info in self.process_manager.info_process.items():
-                if psutil.pid_exists(int(pid)):
-                    proc = psutil.Process(int(pid))
-                    if (
-                        not proc.is_running()
-                        or proc.status() == psutil.STATUS_ZOMBIE
-                        and info["auto_start"] == True
-                    ):
-                        print(
-                            f"Process {info['name']} with PID {pid} has stopped. Restarting..."
-                        )
-                        self.process_manager.execute(
-                            info["commands"],
-                            name=info["name"],
-                            auto_start=info["auto_start"],
-                        )
+            # Initialize Process manager
+            process_manager = Process()
+            processes = process_manager.info_process.copy()
+
+            # If there are no processes, wait and continue
+            if not processes:
+                print("No processes to monitor.")
+                await asyncio.sleep(10)
+                continue
+
+            # Check each process status and restart if necessary
+            for pid, info in processes.items():
+                if not psutil.pid_exists(int(pid)) and bool(info["auto_start"]) == True:
+                    print(
+                        f"Process {info['name']} with PID {pid} has stopped. Restarting..."
+                    )
+                    process_manager.execute(
+                        commands=info["commands"],
+                        name=info["name"],
+                        auto_start=info["auto_start"],
+                        technology=info["technology"],
+                    )
+                    del process_manager.info_process[pid]
+                    process_manager._save_data()
+                # Check if process is stopped but is marked as running
+                # TODO: IMPROVE THIS CHECK
+                elif not psutil.pid_exists(int(pid)) and info["status"] == "running":
+                    print(f"Process {info['name']} with PID {pid} has stopped.")
+                    del process_manager.info_process[pid]
+                    process_manager._save_data()
+                else:
+                    print(f"Process {info['name']} with PID {pid} is running.")
+                    continue
 
             await asyncio.sleep(10)  # Monitor every 10 seconds
 
