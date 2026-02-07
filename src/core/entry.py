44084eefc8062e480bbdc5a5f39b0863e5a01f26
katuishi <diogenes_mj@hotmail.com>
2026-02-07 18:19:23 -0400

    refactor: add help descriptions to CLI commands in entry point

--- src/core/entry.py
+++ src/core/entry.py
@@ -14,10 +14,11 @@ from .engine import stop as stop_app
 
 @click.group()
 def cli():
+    """Kesher - Process management application."""
     pass
 
 
-@click.command()
+@click.command(help="Start a new process")
 @click.argument("path", type=str)
 @click.option("--name", type=str, default=None, help="Name of the process")
 @click.option("--auto-start", is_flag=True, help="Enable auto-start for the process")
@@ -25,37 +26,37 @@ def start(path, name, auto_start):
     start_app(path, name, auto_start)
 
 
-@click.command()
+@click.command(help="Stop a running process")
 @click.argument("id", type=str)
 def stop(id):
     stop_app(id)
 
 
-@click.command()
+@click.command(help="Restart a process")
 @click.argument("id", type=str)
 def restart(id):
     restart_app(id)
 
 
-@click.command()
+@click.command(help="Delete a process")
 @click.argument("id", type=str)
 def delete(id):
     delete_app(id)
 
 
-@click.command()
+@click.command(help="Check the status of a process")
 @click.argument("id", type=str)
 def status(id):
     status_app(id)
 
 
-@click.command()
+@click.command(help="View logs of a process")
 @click.argument("id", type=str)
 def log(id):
     log_app(id)
 
 
-@click.command()
+@click.command(help="Open the monitoring TUI interface")
 def monitor():
     KesherTUI().run()
 
