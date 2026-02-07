5fb51c66712aa287f1e986a849a4542f552e2cdf
katuishi <diogenes_mj@hotmail.com>
2026-02-07 01:22:42 -0400

    refactor: clean up command list formatting and streamline stop and log functions

--- src/core/engine.py
+++ src/core/engine.py
@@ -7,30 +7,35 @@ def start(file_path: str, name=str | None, auto_start=False) -> None:
     env = Environment(file_path)
     if not env.file_path:
         print("The specified file does not exist.")
-        return 
-    
+        return
+
     if env.venv_path:
         if env.api_type == "fastapi":
-            commands = [f"{env.venv_path}", "-u", "-m", "fastapi", "dev", f"{file_path}"]
+            commands = [
+                f"{env.venv_path}",
+                "-u",
+                "-m",
+                "fastapi",
+                "dev",
+                f"{file_path}",
+            ]
         elif env.api_type == "flask":
             commands = [f"{env.venv_path}", "-u", f"{file_path}"]
         elif env.api_type == "django":
             commands = [f"{env.venv_path}", "-u", f"{file_path}", "runserver"]
         elif env.api_type == "general":
-            commands =    [f"{env.venv_path}", "-u", f"{file_path}"],
-              
+            commands = ([f"{env.venv_path}", "-u", f"{file_path}"],)
+
         Process().execute(
             commands=commands,
             name=name,
             auto_start=auto_start,
             technology=env.api_type,
-        )    
-           
+        )
+
+
 def stop(id: str) -> None:
-    if "all" == id.lower():
-        Process().stop_all()
-    else:
-        Process().stop(id)
+    Process().stop(id)
 
 
 def restart(id: str) -> None:
@@ -42,7 +47,7 @@ def status(id: str) -> None:
 
 
 def log(id: str) -> None:
-    print(Process().log(id))
+    Process().log(id)
 
 
 def delete(id: str) -> None:
