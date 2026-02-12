91ef777fbb80a5ffe8a38260fc1d245c5f435475
katuishi <diogenes_mj@hotmail.com>
2026-02-11 23:53:43 -0400

    refact: remove Daemon import from core module and update __all__ export

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,7 +1,6 @@
 """This module serves as the core of the application, providing essential classes and functions for process management and daemonization. It includes the Daemon class for running processes in the background, as well as functions for starting, stopping, restarting, and deleting processes. The Process class is also included for representing individual processes."""
 
-from .daemon import Daemon
 from .entry import delete, restart, start, stop
 from .process import Process
 
-__all__ = ["Process", "start", "stop", "restart", "delete", "Daemon"]
+__all__ = ["Process", "start", "stop", "restart", "delete"]
